336ff78f3da8575358896e4db0a2a4e6f364bbc0
pnzrkmpfwgn <kurtknispel9@gmail.com>
2021-07-02 12:28:37 +0300

    Adjusted plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,14 @@
 module.exports = ({ env }) => ({
     upload: {
-        provider: 'google-cloud-storage',
-        providerOptions: {
-            bucketName: env('GCS_BUCKET_NAME'),
-            publicFiles: env('GCS_PUBLIC_FILES'),
-            uniform:env('GCS_UNIFORM'),
-            serviceAccount: env.json('GCS_SERVICE_ACCOUNT'),
-            baseUrl: env('GCS_BASE_URL'),
-            basePath:env('GCS_BASE_PATH'),
-        }
-    }
-})
+      provider: 'google-cloud-storage',
+      providerOptions: {
+        serviceAccount: env.json('GCS_SERVICE_ACCOUNT'),
+        bucketName: env('GCS_BUCKET_NAME'),
+        basePath: env('GCS_BASE_PATH'),
+        baseUrl: env('GCS_BASE_URL'),
+        publicFiles: env('GCS_PUBLIC_FILES'),
+        uniform: env('GCS_UNIFORM'),
+      },
+    },
+    //...
+});
